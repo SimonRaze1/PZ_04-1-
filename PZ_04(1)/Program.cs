971245b6b449ec17c8ea1091040c071150919e66
shirokov.dd2226 <shirokov.dd2226@COM409-L1>
2023-09-20 15:08:28 +0300

    Добавьте файлы проекта.

---
+++ PZ_04(1)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PZ_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] catTypes = { "о", "п", "р", "с", "т", "у" };
+            foreach (string catType in catTypes)
+                Console.WriteLine(catType);
+
+        }
+    }
+}
